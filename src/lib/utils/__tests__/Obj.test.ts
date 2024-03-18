ce4d8473de4e85476705eba9bc0948fee8931a69
hulatime <hulatime@tutanota.com>
2024-03-18 11:03:45 +0000

    test: fix Obj tests

--- src/lib/utils/__tests__/Obj.test.ts
+++ src/lib/utils/__tests__/Obj.test.ts
@@ -13,42 +13,36 @@ describe('Obj', () => {
   });
 
   describe('#convertToCamel', () => {
-    test('converts all the instantiated objects keys to camelCase', () => {
+    test('returns object with camelCased keys from the original object', () => {
       const obj = new Obj({ foo_bar: 'boop' });
-      obj.convertToCamel();
-      expect(obj.original()).toEqual({ fooBar: 'boop' });
+      expect(obj.convertToCamel()).toEqual({ fooBar: 'boop' });
     });
 
-    test('converts all the instantiated objects keys to camelCase, with multiple keys', () => {
+    test('returns object with camelCased keys from the original object when there are multiple keys', () => {
       const obj = new Obj({ foo_bar: 'boop', triangle_floor: 'goop' });
-      obj.convertToCamel();
-      expect(obj.original()).toEqual({ fooBar: 'boop', triangleFloor: 'goop' });
+      expect(obj.convertToCamel()).toEqual({ fooBar: 'boop', triangleFloor: 'goop' });
     });
 
     test('does not modify keys that are already camel cased', () => {
       const obj = new Obj({ fooBar: 'boop', triangleFloor: 'goop' });
-      obj.convertToCamel();
-      expect(obj.original()).toEqual({ fooBar: 'boop', triangleFloor: 'goop' });
+      expect(obj.convertToCamel()).toEqual({ fooBar: 'boop', triangleFloor: 'goop' });
     });
   });
 
   describe('#convertToSnake', () => {
     test('converts all the instantiated objects keys to snakeCase', () => {
       const obj = new Obj({ fooBar: 'boop' });
-      obj.convertToSnake();
-      expect(obj.original()).toEqual({ foo_bar: 'boop' });
+      expect(obj.convertToSnake()).toEqual({ foo_bar: 'boop' });
     });
 
     test('converts all the instantiated objects keys to snakeCase, with multiple keys', () => {
       const obj = new Obj({ fooBar: 'boop', triangleFloor: 'goop' });
-      obj.convertToSnake();
-      expect(obj.original()).toEqual({ foo_bar: 'boop', triangle_floor: 'goop' });
+      expect(obj.convertToSnake()).toEqual({ foo_bar: 'boop', triangle_floor: 'goop' });
     });
 
     test('does not modify keys that are already snake cased', () => {
       const obj = new Obj({ foo_bar: 'boop', triangle_floor: 'goop' });
-      obj.convertToSnake();
-      expect(obj.original()).toEqual({ foo_bar: 'boop', triangle_floor: 'goop' });
+      expect(obj.convertToSnake()).toEqual({ foo_bar: 'boop', triangle_floor: 'goop' });
     });
   });
 });
